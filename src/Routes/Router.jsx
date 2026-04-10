8c544853839a4485d30a3a8b9e9d367da65fee1c
Forhad <forhaduddin823@gmail.com>
2026-04-10 20:04:43 +0600

    testing whether file location is working or not

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -2,11 +2,18 @@ import { createBrowserRouter } from "react-router";
 import HomeLayout from "../Layouts/HomeLayout";
 import Home from "../Pages/Home";
 import CategoryNews from "../Pages/CategoryNews";
+import Login from "../Pages/Login";
+import Register from "../Pages/Register";
 
 const router = createBrowserRouter([
   {
     path: "/",
     Component: HomeLayout,
+    HydrateFallback: () => (
+      <div className="flex items-center justify-center min-h-screen">
+        <span className="loading loading-dots loading-xl"></span>
+      </div>
+    ),
     children: [
       {
         path: "",
@@ -15,13 +22,23 @@ const router = createBrowserRouter([
       {
         path: "/category/:id",
         element: <CategoryNews></CategoryNews>,
-        loader: () => fetch("/news.json")
+        loader: () => fetch("/news.json"),
       },
     ],
   },
   {
     path: "/auth",
     element: <h2>Authentication layout</h2>,
+    children: [
+      {
+        path: "/auth/login",
+        element: <Login></Login>,
+      },
+      {
+        path: "/auth/register",
+        element: <Register></Register>,
+      },
+    ],
   },
   {
     path: "/news",
--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,10 @@ import "./index.css";
 import App from "./App.jsx";
 
 import { RouterProvider } from "react-router";
-import router from "./Router/Router.jsx";
+import router from "./Routes/Router.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router}></RouterProvider>
+      <RouterProvider router={router}></RouterProvider>
   </StrictMode>,
 );
